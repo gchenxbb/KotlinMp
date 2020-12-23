addcbfecf9ab980bdbbf7643d2185f47f4fb9db2
chenguang <chenguang0508@163.com>
2020-12-23 23:26:12 +0800

    anko

---
+++ app/src/main/java/com/kotlin/app/BaseActivity.kt
@@ -0,0 +1,31 @@
+package com.kotlin.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.toast
+
+/**
+ * Activity抽象基类
+ */
+abstract class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutId())
+
+        initView()
+        initListeners()
+    }
+
+    protected fun initView() {
+
+    }
+
+    protected fun initListeners() {
+    }
+
+    protected abstract fun getLayoutId(): Int
+
+    protected fun showToast(msg: String) {
+        runOnUiThread { toast(msg) }
+    }
+}
